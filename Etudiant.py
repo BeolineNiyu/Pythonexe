3830273198c69f8f0cd46f325e7950a2c450ac98
projets <beolineniyukuri1@gmail.com>
2024-11-08 17:31:52 +0200

    crrect

--- Etudiant.py
+++ Etudiant.py
@@ -1,83 +1,63 @@
 
 etudiants = []
 
+
 def ajouter_etudiant():
-    id = int(input("Entrez l'ID de l'étudiant : "))
     nom = input("Entrez le nom de l'étudiant : ")
-    prenom = input("Entrez le prénom de l'étudiant : ")
-    email = input("Entrez l'email de l'étudiant : ")
-    etudiant = {"id": id, "nom": nom, "prenom": prenom, "email": email}
-    etudiants.append(etudiant)
-    print("Étudiant ajouté avec succès.")
+    etudiants.append(nom)
+    print(f"Étudiant '{nom}' ajouté avec succès.")
 
 
 def afficher_etudiants():
     if etudiants:
-        print("\nListe des étudiants :")
-        for etudiant in etudiants:
-            print(f"ID : {etudiant['id']}, Nom : {etudiant['nom']}, Prénom : {etudiant['prenom']}, Email : {etudiant['email']}")
+        print("Liste des étudiants :")
+        for i, etudiant in enumerate(etudiants, 1):
+            print(f"{i}. {etudiant}")
     else:
-        print("\nAucun étudiant dans la liste.")
-
-
-def rechercher_etudiant():
-    critere = input("Rechercher par (id/nom) : ")
-    valeur = int(input("Entrez la valeur à rechercher : "))
-    if critere == "id":
-
-        etudiant_resultat = [ etudiant for etudiant in etudiants if etudiant['id'] == valeur ]
-        print(f"Étudiant trouvé : {etudiant_resultat}")
-        return
-    # print("Étudiant non trouvé.")
-
-    if rechercher_etudiant:
-         input("Le nom doit commencer par une majuscule." )
-         print(" ce n'est pas Le nom doit commencer par une majuscule : ")
-
-    else:
-        print("Le nom de l'étudiant est valide.")
+        print("Aucun étudiant trouvé.")
 
 
 def supprimer_etudiant():
-    critere = input("Supprimer par (id/nom) : ")
-    valeur = input("Entrez la valeur à supprimer : ")
-    if critere == "id":
-                 
-            etudiant_supprimer = [ etudiant for etudiant in etudiants if etudiant['id'] == valeur ]
-            print("Étudiant supprimé avec succès.")
-            return
-   # print("Étudiant non trouvé.")
+    nom = input("Entrez le nom de l'étudiant à supprimer : ")
+    if nom in etudiants:
+        etudiants.remove(nom)
+        print(f"Étudiant '{nom}' supprimé avec succès.")
+    else:
+        print(f"L'étudiant '{nom}' n'existe pas dans la liste.")
+
+
+def rechercher_etudiant():
+    nom = input("Entrez le nom de l'étudiant à rechercher : ")
+    if nom in etudiants:
+        print(f"L'étudiant '{nom}' a été trouvé.")
+    else:
+        print(f"L'étudiant '{nom}' n'existe pas dans la liste.")
+
 
 def menu():
     while True:
-        print("\n--- Menu ---")
+        print("\nMenu :")
         print("1. Ajouter un étudiant")
-        print("2. Afficher tous les étudiants")
-        print("3. Rechercher un étudiant")
-        print("4. Supprimer un étudiant")
+        print("2. Afficher les étudiants")
+        print("3. Supprimer un étudiant")
+        print("4. Rechercher un étudiant")
         print("5. Quitter")
         
-        choix = input("Choisissez une option : ")
+        choix = input("Choisissez une option (1-5) : ")
         
         if choix == '1':
             ajouter_etudiant()
         elif choix == '2':
             afficher_etudiants()
         elif choix == '3':
-            rechercher_etudiant()
-        elif choix == '4':
             supprimer_etudiant()
+        elif choix == '4':
+            rechercher_etudiant()
         elif choix == '5':
-            print("Au revoir !")
+            print("Programme terminé.")
             break
         else:
-            print("Option invalide, veuillez réessayer.")
+            print("Option invalide. Veuillez choisir une option entre 1 et 5.")
 
 
 menu()
-
-    
-
-
-
-    
